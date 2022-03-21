0d765358d04fb3414d0bd760437c13a1f610154f
LeonXZhou <leon.zhou@queensu.ca>
2022-03-21 11:09:05 -0700

    made an project component with description animation

--- react-portfolio/src/components/SingleProject.js
+++ react-portfolio/src/components/SingleProject.js
@@ -9,6 +9,11 @@ export default function SingleProject() {
       onMouseEnter={() => { text.current.classList.toggle("hidden") }}
       onMouseLeave={() => { text.current.classList.toggle("hidden") }}
     >
-      <p className="hidden" ref={text}>a projectasdfasdfasdf<br></br>asdf</p>
+      <div className="text hidden" ref={text}>
+        <h1>Odyssey</h1>
+        <p>This is an app</p>
+        <a href="https://www.w3schools.com">Visit W3Schools.com!</a> <br></br>
+        <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+      </div>
     </div >);
 }
