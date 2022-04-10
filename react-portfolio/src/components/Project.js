b5e40032d34be5112fb1f09dd4cd53fa6823b7e2
LeonXZhou <leon.zhou@queensu.ca>
2022-04-10 15:56:59 -0700

    refactor individual projects to take render dynamically based on props

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,16 +1,43 @@
 import "./Project.css"
 import SingleProject from "./SingleProject"
 export default function Project() {
+  const odyssey = {
+    image: require("../images/odyssey.png"),
+    title: 'Odyssey',
+    description: "An outdoor adventure planning app built using React, Express, NodeJS, and PostgreSQL",
+    demo: "https://www.w3schools.com",
+    code: "https://www.w3schools.com",
+  }
+
+  const projects = [odyssey,odyssey];
+
+  const projectsJSX = projects.map((project) => {
+    return (<SingleProject
+      image={project.image}
+      title={project.title}
+      description={project.description}
+      demoLink={project.demo}
+      codeLink={project.code}>
+    </SingleProject>)
+  })
+
   return (
     <section className="myProjects">
       <h1>My Projects</h1>
       <div className="project-container">
+        {projectsJSX}
+        {/* <SingleProject
+          image={odyssey.image}
+          title={odyssey.title}
+          description={odyssey.description}
+          demoLink={odyssey.demo}
+          codeLink={odyssey.code}>
+        </SingleProject> */}
+        {/* <SingleProject></SingleProject>
         <SingleProject></SingleProject>
         <SingleProject></SingleProject>
         <SingleProject></SingleProject>
-        <SingleProject></SingleProject>
-        <SingleProject></SingleProject>
-        <SingleProject></SingleProject>
+        <SingleProject></SingleProject> */}
       </div>
     </section>
   )
--- react-portfolio/src/components/SingleProject.js
+++ react-portfolio/src/components/SingleProject.js
@@ -1,20 +1,20 @@
 import "./SingleProject.css"
 import { useRef } from "react";
 
-export default function SingleProject() {
+export default function SingleProject(props) {
   const text = useRef(null);
   return (
     <div
-      className="single-project" style={{ backgroundImage: `url(${require("../images/devEnvironment.png")})` }}
+      className="single-project" style={{ backgroundImage: `url(${props.image})` }}
       onMouseEnter={() => { text.current.classList.toggle("hidden") }}
       onMouseLeave={() => { text.current.classList.toggle("hidden") }}
     >
       <div className="text hidden" ref={text}>
-        <h1>Odyssey</h1>
-        <p>This is an app</p>
-        <a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a> 
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <a href={props.demoLink} target="_blank">Demo</a> 
         <br></br>
-        <a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a>
+        <a href={props.codeLink} target="_blank">Source Code</a>
       </div>
     </div >);
 }
